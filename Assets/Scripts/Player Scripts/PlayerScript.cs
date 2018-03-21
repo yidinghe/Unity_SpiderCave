b957b833921ababe6f407636f150732c0abb027c
yiding he <yidinghe2012@gmail.com>
2018-03-20 22:02:52 -0400

    Added Player Script.
    TODO: Finish jump logic.

---
+++ Assets/Scripts/Player Scripts/PlayerScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+	public float moveForce = 20f, jumpForce = 700f, maxVelocity = 4f;
+
+	private bool grounded;
+
+	private Rigidbody2D myBody;
+	private Animator anim;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		InitializeVariables ();
+	}
+
+	void FixedUpdate ()
+	{
+		PlayerWalkKeyboard ();
+	}
+
+	void InitializeVariables ()
+	{
+		myBody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void PlayerWalkKeyboard ()
+	{
+		float forceX = 0f;
+		float forceY = 0f;
+
+		float vel = Mathf.Abs (myBody.velocity.x);
+
+		float h = Input.GetAxisRaw ("Horizontal");
+
+		if (h > 0) {
+			
+			if (vel < maxVelocity) {
+				forceX = moveForce;
+			}
+
+			Vector3 scale = transform.localScale;
+			scale.x = 1f;
+			transform.localScale = scale;
+
+			anim.SetBool ("Walk", true);
+		} else if (h < 0) {
+			
+			if (vel < maxVelocity) {
+				forceX = -moveForce;
+			}
+
+			Vector3 scale = transform.localScale;
+			scale.x = -1f;
+			transform.localScale = scale;
+
+			anim.SetBool ("Walk", true);
+		} else {
+			anim.SetBool ("Walk", false);
+		}
+
+		myBody.AddForce (new Vector2 (forceX, forceY));
+	}
+}
