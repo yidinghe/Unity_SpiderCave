80f490b907094a7a7323d79f91b24f7379df46ea
yiding he <yidinghe2012@gmail.com>
2018-03-21 22:11:00 -0400

    Added Spider shooter and jumper.

---
+++ Assets/Scripts/Spider Scripts/Spider Jumper/SpiderJumper.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderJumper : MonoBehaviour
+{
+
+	public float forceY = 300f;
+
+	private Rigidbody2D myBody;
+	private Animator anim;
+
+	void Awake ()
+	{
+		InitializeVariables ();
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine (Attack ());
+	}
+
+	void InitializeVariables ()
+	{
+		myBody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	IEnumerator Attack ()
+	{
+		yield return new WaitForSeconds (Random.Range (2, 7));
+
+		forceY = Random.Range (250, 550);
+
+		myBody.AddForce (new Vector2(0, forceY));
+		anim.SetBool ("Attack", true);
+
+		yield return new WaitForSeconds (.7f);
+		StartCoroutine (Attack ());
+	}
+
+	void OnTriggerEnter2D (Collider2D target)
+	{
+		if (target.tag == "Ground") {
+			anim.SetBool ("Attack", false);
+		}
+		if (target.tag == "Player") {
+			Destroy (target.gameObject);
+		}
+	}
+}
