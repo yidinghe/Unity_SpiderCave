e73b5361c5a44c8af42a2cbbeae99e3521a2bd6f
yiding he <yidinghe2012@gmail.com>
2018-03-25 21:21:11 -0400

    Added Door and Diamond.

---
+++ Assets/Scripts/Collectable Scripts/Diamond.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diamond : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (Door.instance != null) {
+			Door.instance.collectablesCount++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D target)
+	{
+		if (target.tag == "Player") {
+			Destroy (gameObject);
+			if (Door.instance != null) {
+				Door.instance.DecrementCollectables ();
+			}
+		}
+	}
+}
