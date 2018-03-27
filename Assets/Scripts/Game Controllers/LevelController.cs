01c7fb4503d215c31d75afdd7715e478c7264ed4
yiding he <yidinghe2012@gmail.com>
2018-03-27 09:21:39 -0400

    Added MainMenu and LevelMenu.

---
+++ Assets/Scripts/Game Controllers/LevelController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+
+	public void PlayGame ()
+	{
+		SceneManager.LoadScene ("GamePlay");
+	}
+
+	public void BackToMenu ()
+	{
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
---
+++ Assets/Scripts/Game Controllers/MainMenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+	public void PlayGame ()
+	{
+		SceneManager.LoadScene ("LevelMenu");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+		
+}
