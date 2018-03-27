34e6aa7d63367920e8487253d532d51c58dc46cb
yiding he <yidinghe2012@gmail.com>
2018-03-27 10:32:34 -0400

    Added CameraFollow. Adjust Level1.

---
+++ Assets/Scripts/Camera Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+	private Transform player;
+
+	public float minX, maxX;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.Find ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (player != null) {
+			Vector3 temp = transform.position;
+			temp.x = player.position.x;
+
+			if (temp.x < minX)
+				temp.x = minX;
+
+			if (temp.x > maxX)
+				temp.x = maxX;
+
+			temp.y = player.position.y + 3.2f;
+
+			transform.position = temp;
+		}
+	}
+}
