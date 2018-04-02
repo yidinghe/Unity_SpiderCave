c6b6d008d7e65ff8934858bc3aa362f5538b18a9
yiding he <yidinghe2012@gmail.com>
2018-04-01 21:15:07 -0400

    Added JoyStick.

---
+++ Assets/Scripts/Game Controllers/Joystick.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+	private PlayerScript player;
+
+	void Awake ()
+	{
+		player = GameObject.Find ("Player").GetComponent<PlayerScript> ();
+	}
+
+	public void OnPointerDown (PointerEventData data)
+	{
+		if (gameObject.name == "Left") {
+			if (player != null) {
+				player.SetMoveLeft (true);
+			}
+		} else if (gameObject.name == "Right") {
+			if (player != null) {
+				player.SetMoveLeft (false);
+			}
+		}
+	}
+
+	public void OnPointerUp (PointerEventData data)
+	{
+		if (gameObject.name == "Left") {
+			if (player != null) {
+				player.StopMove ();
+			}
+		} else if (gameObject.name == "Right") {
+			if (player != null) {
+				player.StopMove ();
+			}
+		}
+	}
+}
